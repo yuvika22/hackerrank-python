497f105c2f5e2137a36abd9f49de40cfdb0ebbb7
Yuvika <Yuvi.Shendge@gmail.com>
2020-12-23 11:23:22 -0500

    Completed code for ListComprehensions

---
+++ basic/ListComprehensions.py
@@ -0,0 +1,16 @@
+def myfunction(x, y, z, n):
+    myarray = []
+    for i in range(0, x):
+        for j in range(0, y):
+            for k in range(0, z):
+                if i + j + k != n:
+                    myarray.append([i, j, k])
+    print(myarray)
+
+
+if __name__ == '__main__':
+    x = int(input()) + 1
+    y = int(input()) + 1
+    z = int(input()) + 1
+    n = int(input())
+    myfunction(x, y, z, n)
