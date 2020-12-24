e01820d844feef0123d256aa5b21c7ba823895ed
Yuvika <Yuvi.Shendge@gmail.com>
2020-12-24 13:37:18 -0500

    Completed the code.

--- basic/NestedLists.py
+++ basic/NestedLists.py
@@ -6,22 +6,24 @@ def find_second_low(scores, names_scores):
 
 
 if __name__ == '__main__':
-    scores = [37.21,  37.21, 37.2, 41, 39]
-    names_scores = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
+    # scores = [37.21,  37.21, 37.2, 41, 39]
+    # names_scores = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
+    scores = []
+    names_scores = []
 
-    # for _ in range(int(input())):
-    #     name = input()
-    #     score = float(input())
-    #     scores.append([score])
-    #     names_scores.append([score, name])
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        scores.append(score)
+        names_scores.append([name, score])
 
-    scores.sort()
     names_scores.sort()
+    scores.sort()
 
-    second_low = scores[1]
+    second_Lowest = -1
+    if len(scores) > 1:
+        second_Lowest = scores[1]
 
-    for student in names_scores:
-        if student[1] == second_low:
-            print("{}".format(student[0]))
-
-
+    for names in names_scores:
+        if second_Lowest == names[1]:
+            print(names[0])
